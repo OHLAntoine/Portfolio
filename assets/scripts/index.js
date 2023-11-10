161d5e826475b70d391c62a999933030eb726695
OHLAntoine <antoine.ohl@gmail.com>
2023-11-10 19:54:13 +0100

    Hide & show content

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,9 +1,18 @@
 // Menu disabled after link clicked
+const menu = document.querySelector('.offcanvas_menu');
+const links = document.querySelectorAll('.nav-link');
+const close = document.querySelector('.btn-close')
 
-
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        if (menu.classList.contains('show')) {
+            close.click();
+        }
+    })
+});
 
 // Show on scroll
-const header = document.querySelector('.header');
+const header = document.querySelector('header');
 const home = document.querySelector('#home');
 const about = document.querySelector('#about');
 const education = document.querySelector('#education');
@@ -24,19 +33,17 @@ function scrolling() {
         if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
             about.classList.add('loaded');
         }
-        if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+        if (document.body.scrollTop > 1200 || document.documentElement.scrollTop > 1200) {
             education.classList.add('loaded');
         }
-        if (document.body.scrollTop > 1300 || document.documentElement.scrollTop > 1300) {
+        if (document.body.scrollTop > 1800 || document.documentElement.scrollTop > 1800) {
             skills.classList.add('loaded');
         }
-        if (document.body.scrollTop > 1700 || document.documentElement.scrollTop > 1700) {
+        if (document.body.scrollTop > 2400 || document.documentElement.scrollTop > 2400) {
             projects.classList.add('loaded');
         }
-        if (document.body.scrollTop > 2100 || document.documentElement.scrollTop > 2100) {
+        if (document.body.scrollTop > 3400 || document.documentElement.scrollTop > 3400) {
             contact.classList.add('loaded');
-        }
-        if (document.body.scrollTop > 2100 || document.documentElement.scrollTop > 2100) {
             footer.classList.add('loaded');
         }
     } else {
@@ -54,8 +61,6 @@ function scrolling() {
         }
         if (document.body.scrollTop > 3000 || document.documentElement.scrollTop > 3000) {
             contact.classList.add('loaded');
-        }
-        if (document.body.scrollTop > 3000 || document.documentElement.scrollTop > 3000) {
             footer.classList.add('loaded');
         }
     }
