89d6b46c594da5b0c2813c6ad9b712cf62bc4b51
OHLAntoine <antoine.ohl@gmail.com>
2023-11-11 16:48:31 +0100

    Ajustement apparition onScroll

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -46,22 +46,73 @@ function scrolling() {
             contact.classList.add('loaded');
             footer.classList.add('loaded');
         }
-    } else {
-        if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+    } else if (window.matchMedia("(min-width: 767px)").matches) {
+        if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
             about.classList.add('loaded');
         }
-        if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+        if (document.body.scrollTop > 1500 || document.documentElement.scrollTop > 1500) {
             education.classList.add('loaded');
         }
-        if (document.body.scrollTop > 1800 || document.documentElement.scrollTop > 1800) {
+        if (document.body.scrollTop > 2500 || document.documentElement.scrollTop > 2500) {
             skills.classList.add('loaded');
         }
-        if (document.body.scrollTop > 2400 || document.documentElement.scrollTop > 2400) {
+        if (document.body.scrollTop > 3600 || document.documentElement.scrollTop > 3600) {
             projects.classList.add('loaded');
         }
-        if (document.body.scrollTop > 3000 || document.documentElement.scrollTop > 3000) {
+        if (document.body.scrollTop > 4700 || document.documentElement.scrollTop > 4700) {
             contact.classList.add('loaded');
             footer.classList.add('loaded');
         }
+    } else if (window.matchMedia("(min-width: 553px)").matches) {
+            if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+                about.classList.add('loaded');
+            }
+            if (document.body.scrollTop > 1500 || document.documentElement.scrollTop > 1500) {
+                education.classList.add('loaded');
+            }
+            if (document.body.scrollTop > 2500 || document.documentElement.scrollTop > 2500) {
+                skills.classList.add('loaded');
+            }
+            if (document.body.scrollTop > 3600 || document.documentElement.scrollTop > 3600) {
+                projects.classList.add('loaded');
+            }
+            if (document.body.scrollTop > 5200 || document.documentElement.scrollTop > 5200) {
+                contact.classList.add('loaded');
+                footer.classList.add('loaded');
+            }
+    } else if (window.matchMedia("(min-width: 475px)").matches) {
+            if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+                about.classList.add('loaded');
+            }
+            if (document.body.scrollTop > 1500 || document.documentElement.scrollTop > 1500) {
+                education.classList.add('loaded');
+            }
+            if (document.body.scrollTop > 2800 || document.documentElement.scrollTop > 2800) {
+                skills.classList.add('loaded');
+            }
+            if (document.body.scrollTop > 3800 || document.documentElement.scrollTop > 3800) {
+                projects.classList.add('loaded');
+            }
+            if (document.body.scrollTop > 5200 || document.documentElement.scrollTop > 5200) {
+                contact.classList.add('loaded');
+                footer.classList.add('loaded');
+            }
+        } else {
+            if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+                about.classList.add('loaded');
+            }
+            if (document.body.scrollTop > 1500 || document.documentElement.scrollTop > 1500) {
+                education.classList.add('loaded');
+            }
+            if (document.body.scrollTop > 3000 || document.documentElement.scrollTop > 3000) {
+                skills.classList.add('loaded');
+            }
+            if (document.body.scrollTop > 4000 || document.documentElement.scrollTop > 4000) {
+                projects.classList.add('loaded');
+            }
+            if (document.body.scrollTop > 5200 || document.documentElement.scrollTop > 5200) {
+                contact.classList.add('loaded');
+                footer.classList.add('loaded');
+            }
+        }
     }
-}
